416ee93a31a3f4c21a33163c8dc31b34f16b0373
Marcos Silva <vinicius_marcosmartins@hotmail.com>
2022-11-20 01:31:40 -0300

    fix: fixing cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_pyfile('settings/config.py')
 db = SQLAlchemy(app)
+CORS(app)
 
 ma = Marshmallow(app)
 
